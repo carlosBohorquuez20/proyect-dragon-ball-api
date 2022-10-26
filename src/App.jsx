2b5d46f2a9b6ce3197054048fda76d12f92f9a98
carlosbohorquez20 <carlos209504@gmail.com>
2022-10-26 15:38:43 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,62 @@
+import { useState, useEffect } from 'react'
+import reactLogo from './assets/react.svg'
+import axios from 'axios'
+import './App.css'
+
+function App() {
+
+  const [character, setCharacter] = useState({});
+
+  const indexRandom  = (Math.floor(Math.random() * 128));
+  useEffect(() => {
+    axios
+      .get("https://dragon-ball-super-api.herokuapp.com/api/characters")
+      .then((res) => setCharacter(res.data[indexRandom]))
+  }, []);
+
+  const nextButton = (() => {
+    
+    const indexRandom  = (Math.floor(Math.random() * 128));
+    axios
+    .get("https://dragon-ball-super-api.herokuapp.com/api/characters")
+    .then((res) => setCharacter(res.data[indexRandom]))
+
+    
+  })
+
+  console.log(character);
+  return (
+    <div className="App">
+      <div className="card-box">
+        <div className='name-character'>
+          <span className='icon-right'><img className='img-icon' src="https://www.seekpng.com/png/full/127-1271955_anime-icon-goku-dragon-ball-z-symbol.png" alt="" /></span><h2>{character.name}</h2><span className='icon-left'><img className='img-icon' src="https://www.seekpng.com/png/full/127-1271955_anime-icon-goku-dragon-ball-z-symbol.png" alt="" /></span>
+        </div>
+        <div className='character-contet'>
+          <div className='left-content'>
+            <img className='character-img' src={character.imageUrl} alt="" />
+          </div>
+          <div className='right-content'>
+            <div className='content-detail'>
+              <div className='title-details'>
+                <h4>Character details</h4>
+              </div>
+              <div className='details'>
+                <p>Origin planet {":"} {character.originplanet}</p>
+                <p>Role  {":"} {character.role}</p>
+                <p>Specie {":"} {character.specie}</p>
+                <p>Status {":"} {character.status}</p>
+                <p>Universe {":"} {character.universe}</p>
+              </div>
+              <div className='btn-next'>
+                <button className='btn' onClick={nextButton}><i className="fa-solid fa-circle-arrow-right icon-next"></i></button>
+              </div>
+              
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
